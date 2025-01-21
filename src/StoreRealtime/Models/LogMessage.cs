8d6a02dc6747ab23420b161b781ecd2e64988102
Bruno Capuano <bcapuano@gmail.com>
2025-01-21 13:09:23 -0500

    Update logging functionality in Home.razor
    Changed the information log title to "Realtime Audio Log" and updated the messages list to use a new `LogMessage` class for structured logging. The `AddMessageAsync` method now creates `LogMessage` objects, which include timestamps. Introduced `LogMessage` class in `LogMessage.cs` to encapsulate log entries with message and date/time properties.

---
+++ src/StoreRealtime/Models/LogMessage.cs
@@ -0,0 +1,16 @@
+﻿namespace StoreRealtime.Models;
+public class LogMessage
+{
+    public LogMessage(string message) {
+        Message = message;
+        DateTime = DateTime.Now;
+    }
+
+    public string Message { get; set; } = string.Empty;
+    public DateTime DateTime { get; set; } = DateTime.Now;
+
+    public override string ToString()
+    {
+        return DateTime.ToString("G") + " - " + Message;
+    }
+}
